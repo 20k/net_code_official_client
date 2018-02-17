4a2a9de38d49693c2fb566209955d20f5c4a6d19
James <Icedshot@gmail.com>
2018-02-17 01:24:13 +0000

    cleanup

--- http_beast_client.cpp
+++ http_beast_client.cpp
@@ -15,199 +15,20 @@
 //
 //------------------------------------------------------------------------------
 
+
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <cstdlib>
-#include <functional>
 #include <iostream>
-#include <memory>
 #include <string>
 
 #include "../crapmud/script_util_shared.hpp"
 
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
-
-//------------------------------------------------------------------------------
-
-#if 0
-// Report a failure
-void
-fail(boost::system::error_code ec, char const* what)
-{
-    std::cerr << what << ": " << ec.message() << "\n";
-}
-
-// Performs an HTTP GET and prints the response
-class session : public std::enable_shared_from_this<session>
-{
-    tcp::resolver resolver_;
-    tcp::socket socket_;
-    boost::beast::flat_buffer buffer_; // (Must persist between reads)
-    http::request<http::string_body> req_;
-    http::response<http::string_body> res_;
-
-public:
-    // Resolver and socket require an io_context
-    explicit
-    session(boost::asio::io_context& ioc)
-        : resolver_(ioc)
-        , socket_(ioc)
-    {
-    }
-
-    // Start the asynchronous operation
-    void
-    run(
-        char const* host,
-        char const* port,
-        char const* target,
-        int version,
-        const std::string& command)
-    {
-        // Set up an HTTP GET request message
-        req_.version(version);
-        req_.method(http::verb::get);
-        req_.target(target);
-        req_.set(http::field::host, host);
-        req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        //boost::beast::http::field
-        //req_.set(http::field::body, "test_body");
-
-        req_.set(http::field::content_type, "text/plain");
-        req_.body() = command;
-
-        req_.prepare_payload();
-
-        // Look up the domain name
-        resolver_.async_resolve(
-            host,
-            port,
-            std::bind(
-                &session::on_resolve,
-                shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
-    }
-
-    void
-    on_resolve(
-        boost::system::error_code ec,
-        tcp::resolver::results_type results)
-    {
-        if(ec)
-            return fail(ec, "resolve");
-
-        // Make the connection on the IP address we get from a lookup
-        boost::asio::async_connect(
-            socket_,
-            results.begin(),
-            results.end(),
-            std::bind(
-                &session::on_connect,
-                shared_from_this(),
-                std::placeholders::_1));
-    }
-
-    void
-    on_connect(boost::system::error_code ec)
-    {
-        if(ec)
-            return fail(ec, "connect");
-
-        // Send the HTTP request to the remote host
-        http::async_write(socket_, req_,
-            std::bind(
-                &session::on_write,
-                shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
-    }
-
-    void
-    on_write(
-        boost::system::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        if(ec)
-            return fail(ec, "write");
-
-        // Receive the HTTP response
-        http::async_read(socket_, buffer_, res_,
-            std::bind(
-                &session::on_read,
-                shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
-    }
-
-    void
-    on_read(
-        boost::system::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        if(ec)
-            return fail(ec, "read");
-
-        // Write the message to standard out
-        //std::cout << res_ << std::endl;
-
-        std::cout << res_.body() << std::endl;
-
-        // Gracefully close the socket
-        /*socket_.shutdown(tcp::socket::shutdown_both, ec);
-
-        // not_connected happens sometimes so don't bother reporting it.
-        if(ec && ec != boost::system::errc::not_connected)
-            return fail(ec, "shutdown");*/
-
-        // If we get here then the connection is closed gracefully
-    }
-};
-#endif
-
-//------------------------------------------------------------------------------
-
-#if 0
-int main(int argc, char** argv)
-{
-    // Check command line arguments.
-    if(argc != 4 && argc != 5)
-    {
-        std::cerr <<
-            "Usage: http-client-async <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-            "Example:\n" <<
-            "    http-client-async www.example.com 80 /\n" <<
-            "    http-client-async www.example.com 80 / 1.0\n";
-        return EXIT_FAILURE;
-    }
-    auto const host = argv[1];
-    auto const port = argv[2];
-    auto const target = argv[3];
-    int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
-
-    // The io_context is required for all I/O
-    boost::asio::io_context ioc;
-
-    // Launch the asynchronous operation
-    std::make_shared<session>(ioc)->run(host, port, target, version);
-
-    // Run the I/O service. The call will return when
-    // the get operation is complete.
-    ioc.run();
-
-    return EXIT_SUCCESS;
-}
-#endif
-
 
 std::string handle_up(const std::string& unknown_command)
 {
@@ -228,59 +49,6 @@ std::string handle_up(const std::string& unknown_command)
 
     return unknown_command;
 }
-
-#if 0
-void do_net_send()
-{
-    // The io_context is required for all I/O
-    boost::asio::io_context ioc;
-
-    // These objects perform our I/O
-    tcp::resolver resolver{ioc};
-    tcp::socket socket{ioc};
-
-    // Look up the domain name
-    auto const results = resolver.resolve(host, port);
-
-    // Make the connection on the IP address we get from a lookup
-    boost::asio::connect(socket, results.begin(), results.end());
-
-    // Set up an HTTP GET request message
-    http::request<http::string_body> req{http::verb::get, target, version};
-    req.set(http::field::host, host);
-    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-    /*req.set(http::field::content_type, "text/plain");
-    req.body() = command;
-
-    req.prepare_payload();*/
-
-    // Send the HTTP request to the remote host
-    http::write(socket, req);
-
-    // This buffer is used for reading and must be persisted
-    boost::beast::flat_buffer buffer;
-
-    // Declare a container to hold the response
-    http::response<http::dynamic_body> res;
-
-    // Receive the HTTP response
-    http::read(socket, buffer, res);
-
-    // Write the message to standard out
-    std::cout << res << std::endl;
-
-    // Gracefully close the socket
-    boost::system::error_code ec;
-    socket.shutdown(tcp::socket::shutdown_both, ec);
-
-    // not_connected happens sometimes
-    // so don't bother reporting it.
-    //
-    if(ec && ec != boost::system::errc::not_connected)
-        throw boost::system::system_error{ec};
-}
-#endif
 
 void test_http_client()
 {
@@ -334,45 +102,3 @@ void test_http_client()
         std::cout << res << std::endl;
     }
 }
-
-///problem is we need to login as a user first
-///gotta do this properly sadly
-#if 0
-void test_http_client()
-{
-    std::string host = "127.0.0.1";
-    std::string port = "6750";
-    std::string target = "/test.txt";
-    std::string command = "user i20k";
-
-    //std::string command = "#up newscript";
-
-    command = handle_up(command);
-
-    std::cout << "exec " << command << std::endl;
-
-    boost::asio::io_context ioc;
-
-    std::shared_ptr<session> var = std::make_shared<session>(ioc);
-
-
-    while(1)
-    {
-        std::string next_command;
-
-        std::getline(std::cin, next_command);
-
-        next_command = handle_up(next_command);
-
-        var->run(host.c_str(), port.c_str(), target.c_str(), 11, next_command);
-
-        std::cout << "block\n";
-
-        ioc.run();
-
-        std::cout << "done\n";
-
-        //printf("hi\n");
-    }
-}
-#endif
