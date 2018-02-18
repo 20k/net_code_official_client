58a55cd859935bb33ecfe3b5147be92836c534f9
James <Icedshot@gmail.com>
2018-02-18 00:25:48 +0000

    untested queue

--- http_beast_client.hpp
+++ http_beast_client.hpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <deque>
+#include <mutex>
 
 /*struct request
 {
@@ -60,6 +62,74 @@ std::string read_file(const std::string& file)
 
     return str;
 }
+
+struct shared_data
+{
+    std::deque<std::string> read_queue;
+    std::deque<std::string> write_queue;
+
+    std::mutex ilock;
+
+    void make_lock()
+    {
+        ilock.lock();
+    }
+
+    void make_unlock()
+    {
+        ilock.unlock();
+    }
+
+    bool has_front_read()
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        return read_queue.size() > 0;
+    }
+
+    bool has_front_write()
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        return write_queue.size() > 0;
+    }
+
+    std::string get_front_read()
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        std::string ret = read_queue.front();
+
+        read_queue.pop_front();
+
+        return ret;
+    }
+
+    std::string get_front_write()
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        std::string ret = write_queue.front();
+
+        write_queue.pop_front();
+
+        return ret;
+    }
+
+    void add_front_write(const std::string& str)
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        write_queue.push_back(str);
+    }
+
+    void add_front_read(const std::string& str)
+    {
+        std::lock_guard<std::mutex> lk(ilock);
+
+        read_queue.push_back(str);
+    }
+};
 
 void test_http_client();
 
--- scripts/i20k.newscript.js
+++ scripts/i20k.newscript.js
@@ -4,7 +4,11 @@ function(context, args)
 
 	#hs.i20k.test_freeze;
 	
-	return fs_call("i20k.test_freeze")();
+	//return ms_call("i20k.test_freeze")();
+	
+	JSON = "hi";
+	
+	return JSON;
 	
 	//return #fs.i20k.test_freeze();
 	
