e2a77fc362410074aabff5c83116f47c320a9f45
James <Icedshot@gmail.com>
2018-02-18 01:47:49 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -10,36 +10,6 @@
 
 #include <crapmud/script_util_shared.hpp>
 
-/*void render_font(sf::RenderWindow& win, const std::string& to_render, vec2f pos, vec4f col, float scale = 1)
-{
-    static sf::Font font;
-    static bool loaded = false;
-
-    if(!loaded)
-    {
-        font.loadFromFile("VeraMono.ttf");
-        loaded = true;
-    }
-
-    sf::Text txt;
-    txt.setFont(font);
-    txt.setString(to_render.c_str());
-    txt.setCharacterSize(round(16 * scale));
-    //txt.setScale(0.5f, 0.5f);
-
-    pos = round(pos);
-
-    txt.setPosition(pos.x(), pos.y());
-
-    col = col * 255;
-
-    txt.setFillColor(sf::Color(col.x(), col.y(), col.z(), col.w()));
-
-    txt.setOrigin(round(txt.getLocalBounds().width/2.f), 0.f);
-
-    win.draw(txt);
-}*/
-
 struct terminal
 {
     std::string command;
