da0e9e8396cad1674d741998e4076a1e160e9c35
James <Icedshot@gmail.com>
2018-04-30 08:46:49 +0100

    build release system

--- build_release.bat
+++ build_release.bat
@@ -6,3 +6,8 @@ cv2pdb.exe bin/Deploy/crapmud_client_strip.exe
 rename "bin\\Deploy\\crapmud_client_strip.pdb" crapmud_client.pdb
 copy "bin\\Deploy\\crapmud_client.pdb" "Release\\bin"
 del "bin\\Deploy\\crapmud_client.pdb"
+
+copy "deps\\freetype\\FTL.TXT" "Release\\bin"
+copy "deps\\imgui\\LICENSE.txt" "Release\\bin"
+copy "deps\\imgui-sfml\\LICENSE" "Release\\bin"
+copy "deps\\freetype\\win64\\freetype.dll" "Release\\bin"
