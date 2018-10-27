8146664971a2d08059743d09772e2cde93d0511e
James <Icedshot@gmail.com>
2018-10-27 06:02:42 +0100

    old code unnecessary now with srgb transforms

--- main.cpp
+++ main.cpp
@@ -198,14 +198,6 @@ int main()
 
     ImGui::SetStyleSrgb(window_ctx.is_srgb);
 
-    /*vec3f bg_srgb = {35, 35, 35};
-    vec3f bg_nosrgb = {30, 30, 30};
-
-    vec3f bg_col = bg_srgb;
-
-    if(!window_ctx.is_srgb)
-        bg_col = bg_nosrgb;*/
-
     vec3f bg_col = {30, 30, 30};
 
     ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(bg_col.x()/255.f, bg_col.y()/255.f, bg_col.z()/255.f, 255/255.f));
@@ -796,14 +788,6 @@ int main()
 
         if(window_ctx.srgb_dirty)
         {
-            /*vec3f bg_col = bg_srgb;
-
-            if(!window_ctx.is_srgb)
-                bg_col = bg_nosrgb;*/
-
-            //ImGui::PopStyleColor(ImGuiCol_WindowBg);
-            //ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(bg_col.x()/255.f, bg_col.y()/255.f, bg_col.z()/255.f, 255/255.f));
-
             text_editor.set_is_srgb(window_ctx.is_srgb);
             ImGui::SetStyleSrgb(window_ctx.is_srgb);
         }
