0567c8e1b9f751defd99883a70f891fe95e0bd0c
James <Icedshot@gmail.com>
2019-09-10 20:21:59 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,16 @@
 
 # harvest dlls sdkencryptedappticket64.dll ncclient.dll steam_api64.dll
 
+rm ./build_root/*.dll
+rm ./build_root/*.exe
+
+mkdir ./Release
+mkdir ./Release/bin
+rm -r ./Release/bin/*
+
+cp ./deps/steamworks_sdk_142/sdk/redistributable_bin/win64/steam_api64.dll ./build_root
 cp ./deps/libncclient/ncclient.dll ./build_root
-cp ./bin/Release/crapmud_client.exe ./build_root
+cp ./bin/Deploy/crapmud_client.exe ./build_root
 
 cd build_root
 
