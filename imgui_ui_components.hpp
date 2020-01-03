613817ed236cb38fd2989410fb9d487e2770f6c5
James <Icedshot@gmail.com>
2020-01-03 16:59:44 +0000

    minor cleanup

--- imgui_ui_components.hpp
+++ imgui_ui_components.hpp
@@ -142,9 +142,6 @@ struct terminal_imgui : serialisable, cacheable, frameable, free_function
 
     ///returns -1 on none
     int get_id_of_focused_realtime_window();
-
-private:
-    ImGuiID last_id = 0;
 };
 
 void clear_everything(terminal_imgui& term, chat_window& chat);
