6da99d84b849175e6275ae862d0d953cdbd842a2
James <Icedshot@gmail.com>
2020-05-06 05:48:10 +0100

    update

--- emmake/serv_impl.py
+++ emmake/serv_impl.py
@@ -1,8 +1,32 @@
-import BaseHTTPServer, SimpleHTTPServer
+#import BaseHTTPServer, SimpleHTTPServer
 
 
-SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] =  'application/wasm'
-SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.js'] =  'text/javascript'
-port = 8000
-httpd = BaseHTTPServer.HTTPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
+#SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] =  'application/wasm'
+#SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.js'] =  'application/ecmascript'
+#port = 8000
+#httpd = BaseHTTPServer.HTTPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
+#httpd.serve_forever()
+
+import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8080
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+	'.html': 'text/html',
+        '.png': 'image/png',
+	'.jpg': 'image/jpg',
+	'.svg':	'image/svg+xml',
+	'.css':	'text/css',
+	'.js':	'application/x-javascript',
+	'': 'application/octet-stream', # Default
+    }
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
 httpd.serve_forever()
